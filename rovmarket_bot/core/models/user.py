dafb5ce2c7fda848f7b1a27944cd259c591bb840
DanilSJ <danilchagarnoy@mail.ru>
2025-08-02 14:01:39 +0300

    fix bugs

--- rovmarket_bot/core/models/user.py
+++ rovmarket_bot/core/models/user.py
@@ -1,10 +1,13 @@
-from sqlalchemy import DateTime, Table, ForeignKey, Column, Integer, String
-from sqlalchemy.orm import Mapped, relationship, mapped_column
-from datetime import datetime
+from sqlalchemy import DateTime, String
+from sqlalchemy.orm import Mapped, mapped_column
+from datetime import datetime, timezone
 from .base import Base
 
 
 class User(Base):
     __tablename__ = "user"
 
-    telegram_id: Mapped[int]
+    telegram_id: Mapped[int] = mapped_column(unique=True, nullable=False)
+    username: Mapped[str | None] = mapped_column(String, nullable=True)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc),
+                                                 nullable=False)
