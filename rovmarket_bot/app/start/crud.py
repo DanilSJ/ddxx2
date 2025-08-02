1e3afba672f0e1cbff36a2787c5ceea6cf581bbe
DanilSJ <danilchagarnoy@mail.ru>
2025-08-02 14:01:59 +0300

    add in add_user argument username

--- rovmarket_bot/app/start/crud.py
+++ rovmarket_bot/app/start/crud.py
@@ -1,8 +1,8 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from rovmarket_bot.core.models.user import User
 
-async def add_user(telegram_id: int, session: AsyncSession) -> User:
-    user = User(telegram_id=telegram_id)
+async def add_user(telegram_id: int, username: str | None, session: AsyncSession) -> User:
+    user = User(telegram_id=telegram_id, username=username)
     session.add(user)
     await session.commit()
     await session.refresh(user)
