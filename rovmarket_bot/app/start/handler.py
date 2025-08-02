8ff3d1cbc6f315e70c1f7d440d7624a6ed0fe67d
DanilSJ <danilchagarnoy@mail.ru>
2025-08-02 13:38:07 +0300

    Connect keyboard.py, communication with the message

--- rovmarket_bot/app/start/handler.py
+++ rovmarket_bot/app/start/handler.py
@@ -1,10 +1,10 @@
 from aiogram import Router
 from aiogram.filters import CommandStart
 from aiogram.types import Message
-
+from .keyboard import menu_start
 
 router = Router()
 
 @router.message(CommandStart())
 async def cmd_start(message: Message):
-    await message.answer("Добро пожаловать!")
+    await message.answer("Добро пожаловать в РовенМаркет! Что вы хотите сделать?", reply_markup=menu_start)
