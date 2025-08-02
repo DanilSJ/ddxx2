e82b3ce1d1e1462cd4163c2c763d55f9a3da7102
DanilSJ <danilchagarnoy@mail.ru>
2025-08-02 13:55:12 +0300

    add func add_user

--- rovmarket_bot/app/start/handler.py
+++ rovmarket_bot/app/start/handler.py
@@ -1,10 +1,15 @@
 from aiogram import Router
 from aiogram.filters import CommandStart
 from aiogram.types import Message
+from rovmarket_bot.core.models import db_helper
 from .keyboard import menu_start
+from .crud import add_user
 
 router = Router()
 
 @router.message(CommandStart())
 async def cmd_start(message: Message):
+    async with db_helper.session_factory() as session:
+        await add_user(telegram_id=message.from_user.id, session=session)
+
     await message.answer("Добро пожаловать в РовенМаркет! Что вы хотите сделать?", reply_markup=menu_start)
