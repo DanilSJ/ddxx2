76973cf3b4449194e1d933ad9d92ff1490eaae43
DanilSJ <danilchagarnoy@mail.ru>
2025-08-02 13:44:53 +0300

    transferring buttons

--- rovmarket_bot/app/start/keyboard.py
+++ rovmarket_bot/app/start/keyboard.py
@@ -3,7 +3,11 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 menu_start = InlineKeyboardMarkup(inline_keyboard=[
     [
         InlineKeyboardButton(text="🔍 Найти объявление",  callback_data="find"),
+        ],
+    [
         InlineKeyboardButton(text="📢 Разместить объявление", callback_data="post"),
+    ],
+    [
         InlineKeyboardButton(text="⚙️ Настройки", callback_data="settings"),
     ]
 ])
