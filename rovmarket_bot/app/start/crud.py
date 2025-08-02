cd4d8ddbcf3ff87e6d40b0eceae3a78069ffd3ce
DanilSJ <danilchagarnoy@mail.ru>
2025-08-03 00:21:11 +0300

    added a condition for verifying the user's existence

--- rovmarket_bot/app/start/crud.py
+++ rovmarket_bot/app/start/crud.py
@@ -1,7 +1,15 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from rovmarket_bot.core.models.user import User
+from sqlalchemy.future import select
+
 
 async def add_user(telegram_id: int, username: str | None, session: AsyncSession) -> User:
+    result = await session.execute(select(User).where(User.telegram_id == telegram_id))
+    user = result.scalars().first()
+
+    if user:
+        return user
+
     user = User(telegram_id=telegram_id, username=username)
     session.add(user)
     await session.commit()
