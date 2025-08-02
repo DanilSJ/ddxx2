1cab229b684a6d118acf24a91452c96682f675e6
DanilSJ <danilchagarnoy@mail.ru>
2025-08-02 13:44:02 +0300

    Fix bug starting

--- rovmarket_bot/main.py
+++ rovmarket_bot/main.py
@@ -1,14 +1,19 @@
 import asyncio
 from aiogram import Dispatcher
 from app.start.handler import router as start
+from core.config import bot
 
 dp = Dispatcher()
 
 async def main():
     dp.include_router(start)
 
+    await asyncio.gather(
+        dp.start_polling(bot),
+    )
 
 if __name__ == "__main__":
+    print("Starting...")
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
